21863fb1dd3325a81cd0a3053ea1863ca3eccfad
iamMHasan <abcimhasanbro@gmail.com>
2022-11-08 09:38:09 +0600

    review section added

--- src/components/Reviewform.js
+++ src/components/Reviewform.js
@@ -3,13 +3,31 @@ import { useContext } from 'react';
 import { AuthContext } from '../context/AuthProvider';
 
 const Reviewform = () => {
-    const {user} = useContext(AuthContext)
-    console.log(user);
+    const { user } = useContext(AuthContext)
+    const { displayName, email, photoURL } = user
+
+    const handleReview = e => {
+        e.preventDefault()
+        const name = e.target.name.value 
+        const email = e.target.email.value 
+        const message = e.target.message.value
+        console.log(name, email, message);
+    }
     return (
-        <form className='m-2'>
-            <h3>Please submit your review</h3>
-           <textarea name='message' className='p-3 rounded mr-3 w-full h-24' placeholder='your message' id="" cols="30" rows="10"></textarea>
-           <button className='btn btn-ghost'>Submit</button>
+        <form onSubmit={handleReview}>
+            <div className='flex items-center py-3'>
+                <p className='mr-3'>Name</p>
+                <input name='name' className='w-full p-2 rounded' type="text" defaultValue={displayName} readOnly />
+            </div>
+            <div className='flex items-center py-3'>
+                <p className='mr-3'>Email</p>
+                <input name='email' className='w-full p-2 rounded' type="text" defaultValue={email} readOnly />
+            </div>
+            <div className='flex items-center py-3'>
+                <p className='mr-3'>Message</p>
+                <textarea name='message' className='w-full p-2 rounded' type="text" />
+            </div>
+            <button  className='btn btn-ghost'>SUBMIT</button>
         </form>
     );
 };
